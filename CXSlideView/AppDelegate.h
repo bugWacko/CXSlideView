df104029ba0ea8ac370a2d6bd547defd88e7fbb1
陈晨昕 <bug_wacko@163.com>
2017-06-16 17:51:48 +0800

    Initial Commit

---
+++ CXSlideView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CXSlideView
+//
+//  Created by 陈晨昕 on 2017/6/16.
+//  Copyright © 2017年 bugWacko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CXSlideView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CXSlideView
+//
+//  Created by 陈晨昕 on 2017/6/16.
+//  Copyright © 2017年 bugWacko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
